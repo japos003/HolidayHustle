4dae34aedc93fbfa1808d298f3347bf49964eb0e
Edward Carrasco <carrasco.edw@gmail.com>
2016-11-19 13:29:57 -0800

    Movement script

--- Holiday Hustle/Assets/Movement.cs
+++ Holiday Hustle/Assets/Movement.cs
@@ -10,6 +10,13 @@ public class Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    
-	}
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+            print("UP");
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+            print("DOWN");
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+            print("LEFT");
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+            print("RIGHT");
+    }
 }
