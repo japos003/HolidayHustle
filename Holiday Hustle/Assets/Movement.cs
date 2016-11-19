8e7ec5a2fb347bfffc22cc343ad0ce84d65b537b
Edward Carrasco <carrasco.edw@gmail.com>
2016-11-19 15:55:53 -0800

    Rotate player

--- Holiday Hustle/Assets/Movement.cs
+++ Holiday Hustle/Assets/Movement.cs
@@ -3,6 +3,14 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
+    float speed = 6.0f;
+    float cameraDistance = 10f;
+    Vector3 moveDirection = Vector3.zero;
+    public Camera camera;
+
+    public Transform target;
+
+
 	// Use this for initialization
 	void Start () {
 	    
@@ -10,13 +18,57 @@ public class Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        // Movement
+        
         if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
-            print("UP");
+        {
+            Vector3 newPos = new Vector3(
+                transform.position.x,
+                transform.position.y + 1,
+                transform.position.z);
+            transform.position = newPos;
+        }
         else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
-            print("DOWN");
+        {
+            Vector3 newPos = new Vector3(
+                transform.position.x,
+                transform.position.y - 1,
+                transform.position.z);
+            transform.position = newPos;
+        }
         else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
-            print("LEFT");
+        {
+            Vector3 newPos = new Vector3(
+                transform.position.x - 1,
+                transform.position.y,
+                transform.position.z);
+            transform.position = newPos;
+        }
         else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
-            print("RIGHT");
+        {
+            Vector3 newPos = new Vector3(
+                transform.position.x + 1, 
+                transform.position.y, 
+                transform.position.z);
+            transform.position = newPos;
+        }
+
+        // Move to new position
+        
+
+        // Shoot
+        if (Input.GetKeyDown(KeyCode.Space))
+            print("SHOOT");
+
+        // Rotate
+        FollowTarget(Input.mousePosition);
+
     }
+
+    void FollowTarget(Vector3 targetPos)
+    {
+        Vector3 mousePosition = camera.ScreenToWorldPoint(new Vector3(targetPos.x, targetPos.y, targetPos.z - camera.transform.position.z));
+        transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2((mousePosition.y - transform.position.y), (mousePosition.x - transform.position.x)) * Mathf.Rad2Deg);
+    }
+
 }
