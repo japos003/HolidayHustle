6393514b6e5f46941bab3943ff4d9723452abe60
japos003 <jrcapostol@gmail.com>
2016-11-19 14:44:05 -0800

    Enemy Class Created
    Enemy now chases the player

--- Holiday Hustle/Assets/EnemyMovement.cs
+++ Holiday Hustle/Assets/EnemyMovement.cs
@@ -3,13 +3,47 @@ using System.Collections;
 
 public class EnemyMovement : MonoBehaviour {
 
+    //Enemy Health
+    //int health = 0;
+    public GameObject player;
+
 	// Use this for initialization
 	void Start () {
-	
+        //health = 100;
+        //player = 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        Debug.Log("Moving object");
+        if(player)
+        {
+            //Must face the player, and will shoot at a specific time
+            Vector3 newPos;
+            if (player.transform.position.x < transform.position.x)
+            {
+                Debug.Log("Right of player");
+                newPos = new Vector3(transform.position.x - 0.01f, transform.position.y, transform.position.z);
+                transform.position = newPos;
+            }
+            else if (player.transform.position.x > transform.position.x)
+            {
+                Debug.Log("Left of player");
+                newPos = new Vector3(transform.position.x + 0.01f, transform.position.y, transform.position.z);
+                transform.position = newPos;
+            }
+            if (player.transform.position.y < transform.position.y)
+            {
+                Debug.Log("Above Player");
+                newPos = new Vector3(transform.position.x, transform.position.y - 0.01f, transform.position.z);
+                transform.position = newPos;
+            }
+            else if (player.transform.position.y > transform.position.y)
+            {
+                Debug.Log("Below Player");
+                newPos = new Vector3(transform.position.x, transform.position.y + 0.01f, transform.position.z);
+                transform.position = newPos;
+            }
+        }
+    }
 }
--- Holiday Hustle/Assets/Movement.cs
+++ Holiday Hustle/Assets/Movement.cs
@@ -11,12 +11,30 @@ public class Movement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
             print("UP");
+            Vector3 newPos = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+            transform.position = newPos;
+        }
+            
         else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
-            print("DOWN");
+        {
+            print("Down");
+            Vector3 newPos = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+            transform.position = newPos;
+        }
+
         else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
             print("LEFT");
+            Vector3 newPos = new Vector3(transform.position.x - 0.5f, transform.position.y, transform.position.z);
+            transform.position = newPos;
+        }
         else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
             print("RIGHT");
+            Vector3 newPos = new Vector3(transform.position.x + 0.5F, transform.position.y, transform.position.z);
+            transform.position = newPos;
+        }
     }
 }
