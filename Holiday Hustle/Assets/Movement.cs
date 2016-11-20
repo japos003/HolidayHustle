eadaa0ef0c50c97a0ae6f958955ea07c89b00ec1
japos003 <jrcapostol@gmail.com>
2016-11-19 16:14:59 -0800

    Adding Eddie's edit to file

--- Holiday Hustle/Assets/Movement.cs
+++ Holiday Hustle/Assets/Movement.cs
@@ -10,31 +10,57 @@ public class Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        // Movement
+
         if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
         {
-            print("UP");
-            Vector3 newPos = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+            Vector3 newPos = new Vector3(
+                transform.position.x,
+                transform.position.y + 1,
+                transform.position.z);
             transform.position = newPos;
         }
-            
         else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
         {
-            print("Down");
-            Vector3 newPos = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+            Vector3 newPos = new Vector3(
+                transform.position.x,
+                transform.position.y - 1,
+                transform.position.z);
             transform.position = newPos;
         }
-
         else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
         {
-            print("LEFT");
-            Vector3 newPos = new Vector3(transform.position.x - 0.5f, transform.position.y, transform.position.z);
+            Vector3 newPos = new Vector3(
+                transform.position.x - 1,
+                transform.position.y,
+                transform.position.z);
             transform.position = newPos;
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
         {
-            print("RIGHT");
-            Vector3 newPos = new Vector3(transform.position.x + 0.5F, transform.position.y, transform.position.z);
+            Vector3 newPos = new Vector3(
+                transform.position.x + 1,
+                transform.position.y,
+                transform.position.z);
             transform.position = newPos;
         }
+
+        // Move to new position
+
+
+        // Shoot
+        if (Input.GetKeyDown(KeyCode.Space))
+            print("SHOOT");
+
+        // Rotate
+        FollowTarget(Input.mousePosition);
+
     }
+
+    void FollowTarget(Vector3 targetPos)
+    {
+        Vector3 mousePosition = camera.ScreenToWorldPoint(new Vector3(targetPos.x, targetPos.y, targetPos.z - camera.transform.position.z));
+        transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2((mousePosition.y - transform.position.y), (mousePosition.x - transform.position.x)) * Mathf.Rad2Deg);
+    }
+}
 }
---
+++ Holiday Hustle/Assets/ProjectileMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileMovement : MonoBehaviour {
+    //public Camera camera;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnBecomeInvisible()
+    {
+        Destroy(this);
+    }
+}
